3cd09269beafedd33909b0c5a847f3cead1c619c
Perezhir000 <hiramperez2003@gmail.com>
2025-01-31 09:14:01 -0700

    added my functions .

--- Src/hal_gpio.c
+++ Src/hal_gpio.c
@@ -1,11 +1,22 @@
 #include <stdint.h>
 #include <stm32f0xx_hal.h>
 #include <stm32f0xx_hal_gpio.h>
-/*
+
 void My_HAL_GPIO_Init(GPIO_TypeDef  *GPIOx, GPIO_InitTypeDef *GPIO_Init)
 {
+    GPIOx->MODER &= ~((1<<16) | (1<<17) | (1<<18) | (1<<19));
+    GPIOx->MODER |= ((1<<18) | (1<<16));
+
+    GPIOx->OTYPER &= ~(0x1 << 8);
+    GPIOx->OTYPER &= ~(0x1 << 9);
+
+    GPIOx->OSPEEDR &= ~(0x1 << 8);
+    GPIOx->OSPEEDR &= ~(0x1 << 9);
+
+    GPIOx->PUPDR &= ~(0x1 << 8);
+    GPIOx->PUPDR &= ~(0x1 << 9);
 }
-*/
+
 
 /*
 void My_HAL_GPIO_DeInit(GPIO_TypeDef  *GPIOx, uint32_t GPIO_Pin)
@@ -20,14 +31,22 @@ GPIO_PinState My_HAL_GPIO_ReadPin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin)
 }
 */
 
-/*
+
 void My_HAL_GPIO_WritePin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState)
 {
-}
-*/
+    GPIOx->ODR &= ~(0x1<< 8); // Clear PIN 8
+    GPIOx->ODR |= (0x1<< 8); // Set PIN 8
 
-/*
+
+    GPIOx->ODR &= ~(0x1<< 9); // Clear PIN 9
+
+}
+
+
+
 void My_HAL_GPIO_TogglePin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin)
 {
+    GPIOx->ODR ^= GPIO_Pin; // Clear PIN 8
+   
 }
-*/
+
--- Src/lab1.c
+++ Src/lab1.c
@@ -13,17 +13,17 @@ int lab1_main(void)
     GPIO_MODE_OUTPUT_PP,
     GPIO_SPEED_FREQ_LOW,
     GPIO_NOPULL};
-    HAL_GPIO_Init(GPIOC, &initStr); // Initialize pins PC8 & PC9
-    assert(GPIOC->MODER == 0x50000); // check if GPIOC->MODER is set to 0
-    assert(GPIOC->OTYPER == 0x0); // check value for OTYPER
-    assert(GPIOC->OSPEEDR == 0x00); // Check value for OSPEEDR 
-    assert(GPIOC->PUPDR == 0x00);
-    assert(GPIOC->ODR == 0x00);
-    HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_SET); // Start PC8 high
+    My_HAL_GPIO_Init(GPIOC, &initStr); // Initialize pins PC8 & PC9
+     //assert(GPIOC->MODER == 0x50000); // check if GPIOC->MODER is set to 0
+    //assert(GPIOC->OTYPER == 0x0); // check value for OTYPER
+    //assert(GPIOC->OSPEEDR == 0x00); // Check value for OSPEEDR 
+    //assert(GPIOC->PUPDR == 0x00); // Check value for PUPDR
+    //assert(GPIOC->ODR == 0x00); // Check value for ODR
+    My_HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_SET); // Start PC8 high
     while (1) 
     {
         HAL_Delay(200); // Delay 200ms
         // Toggle the output state of both PC8 and PC9
-        HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_8 | GPIO_PIN_9);
+        My_HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_8 | GPIO_PIN_9);
     }
 }
