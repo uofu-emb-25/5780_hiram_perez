aeb2c51d86d53865a25ec4be20a5ca73d903f616
Perezhir000 <hiramperez2003@gmail.com>
2025-02-12 09:18:20 -0700

    Checked off 3.3 by manipulating ARR and CCRx values to be close to PWM mode limit, additionally added handler to main.

--- Src/lab3.c
+++ Src/lab3.c
@@ -6,8 +6,15 @@
 void TIM2_IRQHandler(void)
 {
     My_HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_8 | GPIO_PIN_9);
-    TIM2->SR = 0;
+    TIM2->SR &=  ~(0x1);
 }
+
+void TIM3_IRQHandler(void)
+{
+    My_HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_6 | GPIO_PIN_7);
+    TIM3->SR &=  ~(0x1);
+}
+
 
 int lab3_main(void) 
 {
@@ -22,9 +29,15 @@ int lab3_main(void)
     My_HAL_GPIO_InitAll(); // Initialize pins PC6, PC7, PC8 & PC9
     My_HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_SET); // Start PC9 high
 
-
+    GPIOC->MODER &= ~((0x1 << 13) | (0x1 << 12) | (0x1 << 14) | (0x1 << 15)); // Clear PC6 & PC7
+    GPIOC->MODER |= ((0x1 << 15) | (0x1 << 13)); // Set PC6 & PC7 to Alternate function mode
+    GPIOC->AFR[0] &= ~((0x1 << 27) | (0x1 << 26) | (0x1 << 25) | (0x1 << 24)); // Set PC6 to AFO
+    GPIOC->AFR[0] &= ~((0x1 << 31) | (0x1 << 30) | (0x1 << 29) | (0x1 << 28)); // Set PC7 to AFO
     My_TIM2_SETUP();
-    //My_TIM3_SETUP();
+    My_TIM3_SETUP();
     NVIC_EnableIRQ(TIM2_IRQn);
-    NVIC_SetPriority(TIM2_IRQn, 1);   
+    NVIC_SetPriority(TIM2_IRQn, 1);
+    NVIC_EnableIRQ(TIM3_IRQn);   
+    NVIC_SetPriority(TIM3_IRQn, 1);   
+
 }
--- Src/my_time.c
+++ Src/my_time.c
@@ -14,8 +14,8 @@ void My_TIM2_SETUP(void)
 void My_TIM3_SETUP(void)
 {
     RCC->APB1ENR |= (0x1 << 1); // Enable in RCC
-    TIM3->PSC = 1999;
-    TIM3->ARR = 5; // SET frequency to 800 Hz
+    TIM3->PSC = 20;
+    TIM3->ARR = 525; // SET frequency to 800 Hz
     TIM3->CCMR1 &= ~((0x1 << 0) | (0x1 << 1)); // Set TIM3 CC1S to output mode
     TIM3->CCMR1 &= ~((0x1 << 9) | (0x1 << 8)); // Set TIM3 CC2S to output mode
     TIM3->CCMR2 &= ~((0x1 << 0) | (0x1 << 1)); // Set TIM3 CC3S to output mode
@@ -27,9 +27,9 @@ void My_TIM3_SETUP(void)
     TIM3->CCMR2 |= (0x1 << 11); // Set TIM3 Channel 1 preload enable
     TIM3->CCER |= (0x1 << 0); // Set TIM3 Channel 1 output enable bit
     TIM3->CCER |= (0x1 << 4); // Set TIM3 Channel 2 output enable bit
-    TIM3->CCR1 = 1;
-    TIM3->CCR2 = 1;
-    //TIM3->CR1 = TIM_CR1_CEN;
+    TIM3->CCR1 = 500;
+    TIM3->CCR2 = 20;
+    TIM3->CR1 = TIM_CR1_CEN;
 }
 
 
